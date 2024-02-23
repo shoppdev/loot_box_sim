5a10841d048ae68d4956c1967b9197658d94e419
Travis <tegbert@gmail.com>
2024-02-23 14:30:09 -0700

    save/load tweaks

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 # IMPORTS
 import pickle
 import os
+import random
 
 # VARS
 player = None
+path_start = 'files/bin/'
 
 
 # CLASSES
@@ -12,19 +14,24 @@ class Player:
     def __init__(self, name):
         self.name = name
         self.gold_total = 1000  # just a guess, will update for balance
-        # self.invantory = ['basic sword', 'hat']
+        self.inventory = {"Sword" : ['Rare', 10, 0, 4, 9]}
         self.trade_status = False
 
     def __repr__(self):
-        print(self.name, self.gold_total, self.trade_status)
+        pass
 
+    def display_inventory(self):
+        pass
+
+    def display_stats(self):
+        pass
 
 
 
 # this gets player name input and checks to see if save exists
 def check_for_player():
     player_name = input("What is your player name? (Use same name to continue game) ")
-    file_path = 'files/bin/' + player_name
+    file_path = path_start + player_name
 
     # check for file
     if os.path.isfile(file_path):
@@ -46,11 +53,19 @@ def player_create(player_name, path):
     return player
 
 # need to build new path nothing returned
-def player_save(player_name, path, player):
+def player_save(path, player):
     with open(path, 'wb') as file:
         pickle.dump(player, file)
 
+# just to make one thing easier
+def get_path():
+    return path_start + player.name
+
 
 ## MAIN LOOP ##
+# Starts the game asking for player
 player = check_for_player()
-print(player.name, player.gold_total)
+
+
+#saves changes to player
+player_save(get_path(), player)
