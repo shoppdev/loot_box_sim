8e26a24a9e4c817d52eaf4f180542144004053df
Travis <tegbert@gmail.com>
2024-02-24 10:50:44 -0700

    moved Player Class to own file

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 import pickle
 import os
 import random
+from player import Player
 
 # VARS
 player = None
@@ -9,41 +10,41 @@ path_start = 'files/bin/'
 
 
 # CLASSES - move to new file soon
-class Player:
+# class Player:
 
-    def __init__(self, name):
-        self.name = name
-        self.gold_total = 1000  # just a guess, will update for balance
-        self.inventory = {"Basic Sword" : ['Short Sword', 'Common', 10, 1]} # name item rarity max_value num_in_inventory
-        self.trade_status = False
+#     def __init__(self, name):
+#         self.name = name
+#         self.gold_total = 1000  # just a guess, will update for balance
+#         self.inventory = {"Basic Sword" : ['Short Sword', 'Common', 10, 1]} # name item rarity max_value num_in_inventory
+#         self.trade_status = False
 
-    def __repr__(self):
-        pass
+#     def __repr__(self):
+#         pass
 
-    # display inventory and alow you to sell, trade, delete
-    def access_inventory(self):
-        print('\n')
-        print(" NAME            |      TYPE     |      RARITY     |            VALUE     |               QTY")
-        print('_______________________________________________________________________________________________')
-        for key, value in self.inventory.items():
+#     # display inventory and alow you to sell, trade, delete
+#     def access_inventory(self):
+#         print('\n')
+#         print(" NAME            |      TYPE     |      RARITY     |            VALUE     |               QTY")
+#         print('_______________________________________________________________________________________________')
+#         for key, value in self.inventory.items():
             
-            print('"{key}"     |     {type}     |     {rarity}     |     Value: {value}     |     {in_inventory}'.format(key=key, type = value[0], rarity = value[1], value = value[2], in_inventory = value[3]))
-            print('_______________________________________________________________________________________________')
+#             print('"{key}"     |     {type}     |     {rarity}     |     Value: {value}     |     {in_inventory}'.format(key=key, type = value[0], rarity = value[1], value = value[2], in_inventory = value[3]))
+#             print('_______________________________________________________________________________________________')
 
-        print("\n")
-        print("1: Sell")
-        print("2: Trade")
-        print("3: Destroy")
-        print("0: Exit")
-        choice = input("\nWhat would you like to do with your inventory?")
+#         print("\n")
+#         print("1: Sell")
+#         print("2: Trade")
+#         print("3: Destroy")
+#         print("0: Exit")
+#         choice = input("\nWhat would you like to do with your inventory?")
 
-    def add_to_inventory(self, won_inventory):
-        # pass in a dict of all the objects recieved if object in inventory increases qty
-        for key, value in won_inventory.items():
-            if key in self.inventory.keys():
-                self.inventory[key][-1] += 1
-            else:
-                self.inventory[key] = value
+#     def add_to_inventory(self, won_inventory):
+#         # pass in a dict of all the objects recieved if object in inventory increases qty
+#         for key, value in won_inventory.items():
+#             if key in self.inventory.keys():
+#                 self.inventory[key][-1] += 1
+#             else:
+#                 self.inventory[key] = value
         
 # ////// FUNCTIONS ///////
 
