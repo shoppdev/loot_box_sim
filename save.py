a4779167334a08d0399a4cd866d32b312b8c4ca5
Travis <tegbert@gmail.com>
2024-02-28 10:17:45 -0700

    fixed saving to wrong directory

--- save.py
+++ save.py
@@ -1,12 +1,7 @@
 import pickle
 
-# this will work need to pass in the player object, not the name
-
-
-path_start = 'files/bin/'
-
 # saves player file, nothing returned
-def player_save(player, path_start = 'files/bin'):
+def player_save(player, path_start = 'files/bin/'):
     path = path_start + player.name
     with open(path, 'wb') as file:
         pickle.dump(player, file)
